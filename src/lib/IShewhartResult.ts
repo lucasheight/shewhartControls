fcc13fbb24f18b8c4532742f7853918c9c4a915c
Lynden Garrett <lynden.garrett@health.nsw.gov.au>
2020-12-14 14:59:22 +1100

    updated packaging

--- src/lib/IShewhartResult.ts
+++ src/lib/IShewhartResult.ts
@@ -1,3 +1,19 @@
+export interface IViolations {
+  /** 1 Point Outside the +/−3 Sigma Limits */
+  ThreeSigmaViolation: boolean;
+  /** 8 Successive Consecutive Points Above (or Below) the Centerline */
+  EightPointsAboveBelowMean: boolean;
+  /** Six or More Consecutive Points Steadily Increasing or Decreasing */
+  SixPointTrend: boolean;
+  /** Two out of Three Successive Points in Zone A or Beyond */
+  TwoOfThreeOuterThirdViolation: boolean;
+  /** 15 Consecutive Points in Zone C on Either Side of the Centerline */
+  InnerThird15ConsecutiveViolation: boolean;
+}
+export interface IShewhartDataPoint {
+  value: number;
+  violations: IViolations;
+}
 export interface IShewhartResult {
   isValid: boolean;
   mean: number;
@@ -11,32 +27,3 @@ export interface IShewhartResult {
   violations: IViolations;
   points: IShewhartDataPoint[];
 }
-export interface IShewhartDataPoint {
-  value: number;
-  violations: IViolations;
-}
-export interface IViolations {
-  // 1 Point Outside the +/−3 Sigma Limits
-  ThreeSigmaViolation: boolean;
-  // 8 Successive Consecutive Points Above (or Below) the Centerline
-  EightPointsAboveBelowMean: boolean;
-  // Six or More Consecutive Points Steadily Increasing or Decreasing
-  SixPointTrend: boolean;
-  // Two out of Three Successive Points in Zone A or Beyond
-  TwoOfThreeOuterThirdViolation: boolean;
-  // 15 Consecutive Points in Zone C on Either Side of the Centerline
-  InnerThird15ConsecutiveViolation: boolean;
-}
-// export enum ControlFlags {
-//   None = 0,
-//   // 1 Point Outside the +/−3 Sigma Limits
-//   ThreeSigmaViolation = 1 << 0,
-//   // 8 Successive Consecutive Points Above (or Below) the Centerline
-//   EightPointsAboveBelowMean = 1 << 1,
-//   // Six or More Consecutive Points Steadily Increasing or Decreasing
-//   SixPointTrend = 1 << 2,
-//   // Two out of Three Successive Points in Zone A or Beyond
-//   TwoOfThreeOuterThirdViolation = 1 << 3,
-//   // 15 Consecutive Points in Zone C on Either Side of the Centerline
-//   InnerThird15ConsecutiveViolation = 1 << 4,
-// }
